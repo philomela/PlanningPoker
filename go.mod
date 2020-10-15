330245328091eb2bc25d655999a3f090782aac4e
Roman_Solovyev <romaphilomela@yadex.ru>
2020-10-15 05:04:28 +0300

    planning-poker-01 Расширен и добавлен функционал
    Добавлены контроллеры, частично реализованы механизмы создания комнаты, вход. в комнату по id, работа с сессиями, процесс создания новой комнаты

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/philomela/ServerPlanningPoker
+
+go 1.15
+
+require github.com/go-redis/redis v6.15.9+incompatible // indirect
--- startup.go
+++ startup.go
@@ -3,20 +3,44 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
-
-	"net"
+	"log"
+	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
 
 	"./ServerPlanningPoker"
+	"./session"
 	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+const (
+	cookieName = "sessionId"
 )
 
 func main() {
 	var currentServersSettings ServerPlanningPoker.ServersSettings
-	var currentServer net.Listener
-	var err error
 
+	//var err error
+	type connection struct {
+		conn    *websocket.Conn
+		rooomId string
+		UUID    uuid.UUID
+	}
+	var conns []connection
+	type connections []*websocket.Conn
+	//var rooms []Room //Коллекция комнат, вынести в отдельный пакет
+	//collectionRoom := make(map[string]connections)
 	/* Открываем, читаем и парсим json */
 	jsonFile, err := os.Open("serversSettings.json")
 
@@ -28,46 +52,180 @@ func main() {
 
 		byteArrayJsonSettings, _ := ioutil.ReadAll(jsonFile)
 		json.Unmarshal(byteArrayJsonSettings, &currentServersSettings)
-		fmt.Println("Server succsesful configured. ©Roman Solovyev/Andrew Zabolotniy")
 	}
 
-	/* Получаем экземпляры серверов */
-	currentServer, err = ServerPlanningPoker.Server{ProtocolServer: currentServersSettings.ServerPlanningPoker.ProtocolServer, Port: currentServersSettings.ServerPlanningPoker.Port}.GetServer()
+	currentSqlServer, err := ServerPlanningPoker.ServerSql{DSN: currentServersSettings.SQLServer.DSN, TypeSql: currentServersSettings.SQLServer.TypeSql}.OpenConnection()
 
-	if err != nil {
-		fmt.Println(err)
-		return //Добавить реализацию логирования ошибок в базу
-	} else {
-		fmt.Println("Server started and listening port: " + currentServersSettings.ServerPlanningPoker.Port)
-	}
+	fmt.Println("Server succsesful configured. ©Roman Solovyev")
 
-	currenSqlServer, err := ServerPlanningPoker.ServerSql{DSN: currentServersSettings.SQLServer.DSN, TypeSql: currentServersSettings.SQLServer.TypeSql}.OpenConnection()
+	http.HandleFunc("/echo/", func(w http.ResponseWriter, r *http.Request) {
+		var URL = r.URL.Query()["roomId"][0]
+		var Conn connection
+		Conn.conn, _ = upgrader.Upgrade(w, r, nil)
+		Conn.rooomId = URL
+		Conn.UUID, err = uuid.NewUUID()
+		if err != nil {
+			fmt.Println(err)
+			return //Добавить реализацию логирования ошибок в базу
+		}
+		currentSqlServer.Query(`EXEC [CreateConnection] @UUID=?, @RoomId=?`, Conn.UUID.String(), Conn.rooomId)
+		conns = append(conns, Conn)
 
-	if err != nil {
-		fmt.Println(err)
-		return //Добавить реализацию логирования ошибок в базу
-	}
+		//collectionRoom[URL] = conns
+		//conn.WriteMessage(1, []byte(URL))
+		//conn.WriteMessage(1, []byte("Hello, I'm TCP server"))
+		println(conns)
+		println(URL)
+		for {
+			msgType, msg, err := Conn.conn.ReadMessage()
+			//Добавить логику работы когда мы закрываем соединение, чтобы удалять ненужные
+			if err != nil {
+				return
+			}
 
-	/* Общаемся с базой */
-	result, err := currenSqlServer.Query("SELECT * FROM test") //Делаем запрос к тестовой базе
-	if err != nil {
-		fmt.Println(err)
-		return //Добавить реализацию логирования ошибок в базу
-	}
-	collection, err := result.Columns()
-	if err != nil {
-		fmt.Println(err)
-		return //Добавить реализацию логирования ошибок в базу
-	}
+			currentSqlServer.Exec("INSERT INTO ServerPlanningPoker.Connections(Id, [GUID], RoomId) VALUES(1, NEWID(), 1)")
+			if err != nil {
+				fmt.Println(err)
+				return //Добавить реализацию логирования ошибок в базу
+			}
+			currentSqlServer.Exec("EXECUTE SaveError 'Error test'")
+			if err != nil {
+				fmt.Println(err)
+				return //Добавить реализацию логирования ошибок в базу
+			}
 
-	fmt.Printf(collection[0]) //Выводим в сообщении, первое название поля из таблицы Test
+			for _, value := range conns {
+				if value.rooomId == URL {
+					msg = []byte(URL)
+					value.conn.WriteMessage(msgType, msg)
+				}
 
-	currenSqlServer.Exec("EXECUTE SaveError 'Error test'")
-	if err != nil {
-		fmt.Println(err)
-		return //Добавить реализацию логирования ошибок в базу
-	}
+			}
 
-	currentServer.Accept()
-	//currentServer.
+			//return
+		}
+	})
+
+	http.HandleFunc("/rooms/", func(w http.ResponseWriter, r *http.Request) {
+		roomId := r.URL.Query()["roomId"][0]
+		if len(roomId) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			//http.Error(w, "Bad Request", http.StatusBadRequest)
+			//http.ServeFile(w, r, "templates/error_bad_request.html") //реализовать вызов методов страниц ошибок
+			return //Реализовать обертку для ошибок
+		}
+		success := "false"
+
+		resultSQL, err := currentSqlServer.Query("SELECT 'true' WHERE EXISTS (SELECT [GUID] FROM ServerPlanningPoker.Rooms WHERE [GUID] = '" + roomId + "')")
+		defer resultSQL.Close()
+		if err != nil {
+			log.Println(err)
+		}
+		for resultSQL.Next() {
+			err := resultSQL.Scan(&success)
+			if err != nil {
+				fmt.Println(err)
+				continue //Добавить реализацию логирования ошибок в базу
+			}
+		}
+		type ViewData struct {
+			Success     string
+			RoomId      string
+			CurrentHost string
+		}
+		data := ViewData{
+			Success:     success,
+			RoomId:      roomId,
+			CurrentHost: currentServersSettings.ServerHost.Host,
+		}
+		fmt.Println(roomId)
+		fmt.Println(success)
+		if success == "true" {
+			//http.Redirect(w, r, "/room/?roomId="+roomId, 301)
+			tmpl, _ := template.ParseFiles("templates/rooms.html")
+			tmpl.Execute(w, data)
+		}
+
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		login := r.FormValue("login")
+		password := r.FormValue("password")
+		if len(login) > 0 && len(password) > 0 {
+			resultSP, err := currentSqlServer.Query(`EXEC [CheckUser] ?, ?`, login, password)
+			if err != nil {
+				log.Println(err)
+			}
+			var resultCkeck bool
+			for resultSP.Next() {
+				err := resultSP.Scan(&resultCkeck)
+				if err != nil {
+					fmt.Println(err)
+					continue //Добавить реализацию логирования ошибок в базу
+				}
+			}
+			if resultCkeck {
+				session := session.NewSession()
+				sessionId := session.InitSession(login)
+				cookie := &http.Cookie{
+					Name:    cookieName,
+					Value:   sessionId,
+					Expires: time.Now().Add(5 * time.Minute),
+				}
+				http.SetCookie(w, cookie)
+				w.Write([]byte("Sucsess"))
+				return
+			}
+		}
+		w.Write([]byte("Wrong"))
+
+	})
+
+	http.HandleFunc("/loginform", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/login.html")
+	})
+
+	http.HandleFunc("/room/", func(w http.ResponseWriter, r *http.Request) {
+
+		http.ServeFile(w, r, "templates/index.html")
+	})
+
+	http.HandleFunc("/NewRoom", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/newPlanningPokerRoom.html")
+	})
+
+	http.HandleFunc("/create-room", func(w http.ResponseWriter, r *http.Request) {
+		nameRoom := r.FormValue("nameRoom")
+		xmlTasks := r.FormValue("xmlTasks")
+		if len(nameRoom) == 0 && len(xmlTasks) == 0 {
+			http.ServeFile(w, r, "templates/error_bad_request.html") //реализовать вызов методов страниц ошибок
+			return
+		}
+		var roomId string
+		fmt.Println(nameRoom)
+		fmt.Println(xmlTasks)
+		//ctx := context.Background()
+		resultSP, err := currentSqlServer.Query(`EXEC [NewPlanningPokerRoom] @NameRoom=?, @Tasks=?;`, nameRoom, xmlTasks)
+
+		if err != nil {
+			log.Println(err)
+		}
+		var hello string
+		for resultSP.Next() {
+			err := resultSP.Scan(&hello)
+			if err != nil {
+				fmt.Println(err)
+				continue //Добавить реализацию логирования ошибок в базу
+			}
+		}
+		fmt.Println(currentServersSettings.ServerHost.Host)
+		w.Write([]byte(currentServersSettings.ServerHost.Host + currentServersSettings.ServerHost.Room + strings.ToLower(hello)))
+		fmt.Println(resultSP)
+		fmt.Println(hello)
+		fmt.Println(roomId)
+
+	})
+
+	http.ListenAndServe(":80", nil)
+
 }
