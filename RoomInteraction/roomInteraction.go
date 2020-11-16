db399555a24366d9c189f8e03d90719c9419cfd4
Roman_Solovyev <romaphilomela@yadex.ru>
2020-11-16 13:07:35 +0300

    planning-poker-01 Add new functional
    Add storage procedure and roomInteraction.go

---
+++ RoomInteraction/roomInteraction.go
@@ -0,0 +1,34 @@
+package RoomInteraction
+
+import (
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+type Connection struct {
+	Conn      *websocket.Conn
+	RoomGUID  string
+	UUID      uuid.UUID
+	UserEmail string
+}
+
+type Change interface {
+	GetChange(nameEvent string, conn *Connection) (commandOnChange string)
+}
+
+type ChangesViewModel struct {
+	Changes map[string]string
+}
+
+func NewChangesViewModel() *ChangesViewModel {
+	ChangesViewModelOut := ChangesViewModel{
+		Changes: make(map[string]string),
+	}
+	ChangesViewModelOut.Changes["ChangeVote"] = `EXEC [Push_And_Get_Changes] @xmlChanges=?, @nameChanges=?, @roomGUID=?, @email=?`
+	return &ChangesViewModelOut
+}
+
+func (c *ChangesViewModel) GetChange(nameEvent string, conn *Connection) (commandOnChange string) {
+	commandOnChange = c.Changes[nameEvent]
+	return
+}
