c281051e4b989712c0670c21fcd7f6cecb4d4ea7
philomela <romaphilomela@yandex.ru>
2021-01-05 22:21:46 +0300

    planning-poker-01. Добавлен функционал управления тасками создателя комнаты. Доработана хранимая процедура обработки изменений

--- templates/js/creatorScripts.js
+++ templates/js/creatorScripts.js
@@ -1,3 +1,15 @@
+var startVotingData;
 $('#start-voting').click(() => {
-    socketInst.send("StartVoting")
+    socketInst.send(`StartVoting==${startVotingData}`)
+    console.log(startVotingData)
 });
+
+$(document).on('click', '.task-room', (e) => {
+    e.preventDefault()
+    startVotingData = `<Change><StartVoting taskId="${e.target.id.split('-')[1]}" isCurrentActive="1"/></Change>`
+    console.log(startVotingData)
+});
+
+$('#finish-voting').click(() => {
+    socketInst.send(`StopVoting==`)
+})
